cb7915ecc1f694c0b033c96a38702e122526398b
otakumesi <bakednt@gmail.com>
2017-09-07 01:15:03 +0900

    remove material

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueMaterial from 'vue-material'
 import App from './App'
-import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
-
-Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
