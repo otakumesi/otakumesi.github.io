c25f34e85a59149f53eb1322fc956a929807d00a
Takuro Niitsuma <takuro.niitsuma@livesense.co.jp>
2018-06-19 13:55:34 +0900

    create Header and Footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer`
+  padding: 10px 30px;
+`;
+
+const CopyRight = styled.div`
+  text-align: center;
+`;
 
 const Footer: React.SFC = () =>
-  <footer>
-    footer
-  </footer>
+  <FooterWrapper>
+    <CopyRight>
+    © 2018 otakumesi
+    </CopyRight>
+  </FooterWrapper>
 
 export default Footer;
--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Logo from '../atoms/Logo';
 
+const MenuWrapper = styled.div`
+`;
+
+const HeaderWrapper = styled.header`
+  padding: 20px 30px 15px;
+  display: flex;
+  justify-content: space-between;
+`;
+
 const Header: React.SFC = () =>
-  <header>
-    <div>
-      <Logo />
-    </div>
-  </header>
+  <HeaderWrapper>
+    <Logo />
+    <MenuWrapper>
+      test
+    </MenuWrapper>
+  </HeaderWrapper>
 
 export default Header;
