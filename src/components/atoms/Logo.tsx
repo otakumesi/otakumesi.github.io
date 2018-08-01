cf1af90f7fd7360387258aabae7f79b366c24640
Takuro Niitsuma <takuro.niitsuma@livesense.co.jp>
2018-08-01 10:18:28 +0900

    自前で設定を作る方針に変更

--- src/components/atoms/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-const Logo: React.SFC = () =>
-  <div className="logo">
-    OTAKUMESI.IO
-  </div>
-
-export default Logo;
--- src/components/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import HomeTemplate from '../templates/HomeTemplate';
-
-const Home: React.SFC = () =>
-  <HomeTemplate />
-
-export default Home;
