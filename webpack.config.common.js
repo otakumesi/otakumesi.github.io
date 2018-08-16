3c7afc0fb8a267a94bfb88c9b3f701f487be9d1d
Takuro Niitsuma <takuro.niitsuma@livesense.co.jp>
2018-08-16 10:18:12 +0900

    update

---
+++ webpack.config.common.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const SRC_PATH = path.resolve(__dirname, 'src/index.tsx')
+const DIST_DIR = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  entry: {
+    app: SRC_PATH,
+    vendors: [
+      'react',
+      'react-dom'
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+}
