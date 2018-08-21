738edebd46a3f7b1b5d0420035aa13d35494c1ac
otakumesi <bakednt@gmail.com>
2018-08-21 21:25:33 +0900

    if -> switch

--- src/contexts/articles/reducer.ts
+++ src/contexts/articles/reducer.ts
@@ -1,13 +1,17 @@
 import { ARTICLE_FETCH, ARTICLE_RECEIVE } from './constants'
 
 const articleReducer = (state: ArticleState, action: ArticleActionType): ArticleState => {
-  if (action.type === ARTICLE_FETCH) {
-    return {...state}
+  switch(action.type) {
+    case ARTICLE_FETCH:
+      return {...state}
+    case ARTICLE_RECEIVE:
+      if(action.payload) {
+        return {...state, ...action.payload }
+      }
+      return state
+    default:
+      return state
   }
-  if (action.type === ARTICLE_RECEIVE && action.payload) {
-    return {...state, ...action.payload }
-  }
-  return state
 }
 
 export default articleReducer
