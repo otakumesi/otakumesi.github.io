a8a5fc18f7c02513310ae4986843141d480050de
Takuro Niitsuma <takuro.niitsuma@livesense.co.jp>
2018-08-21 14:34:52 +0900

    lockの更新

---
+++ src/types/index.d.ts
@@ -0,0 +1,53 @@
+interface QiitaApiItem {
+  title: string
+  body: string
+  url: string
+  updated_at: string
+}
+
+type QiitaApiItems = QiitaApiItem[]
+
+interface ScrapboxApiPage {
+  title: string
+  descriptions: string[]
+  updated: number
+}
+
+interface ScrapboxApiPages {
+  pages: ScrapboxApiPage[]
+}
+
+interface YQLResults<T> {
+  json: T
+}
+
+interface YQLQuery<T> {
+  results: YQLResults<T>
+}
+
+interface YQLApi<T> {
+  query: YQLQuery<T>
+}
+
+interface ArticleActionType {
+  type: string
+  payload: { articles: ArticleStore[] } | undefined
+}
+
+type ArticleActionDispatcher = (action: ArticleActionType) => void
+
+interface ArticleStore {
+  uniqueKey: string
+  media: string
+  date: string
+  title: string
+  description: string
+  url: string
+  color: string;
+}
+
+interface ArticleState {
+  articles: ArticleStore[]
+  dispatch: ArticleActionDispatcher | null | undefined
+}
+
